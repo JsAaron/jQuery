eddda1daea7c29915f367cd926cd6065cd51ce8f
jsAaron <401545553@qq.com>
2014-06-23 11:01:06 +0800

    动画

--- Queue/core.js
+++ Queue/core.js
@@ -1,20 +1,43 @@
 $(function() {
 
-    var foo = $('#foo')
 
-    // foo.slideUp().slideDown().queue(function(fn,o){
-    //     console.log(fn,o)
-    // })
+    var div = $("div");
 
-    foo.slideUp(function(){
-        console.log(1)
-    }).slideDown(function(){
-        console.log(2)
-    }).queue(function() {
-        console.log(3)
-       // $(this).dequeue();
-    });
+    function runIt() {
+
+
+        div.show({
+            'queue':false
+        });
+        // div.slideToggle(1000);
+        // div.slideToggle("fast");
+
+        return
+
+        div.show("slow");
+        div.animate({
+            left: '+=300'
+        }, 2000);
+        div.slideToggle(1000);
+        div.slideToggle("fast");
+        div.animate({
+            left: '-=200'
+        }, 1500);
+        div.hide("slow");
+        div.show(1200);
+        div.slideUp("normal", runIt);
+    }
+
+    function showIt() {
+        var n = div.queue("fx");
+        $("span").text(n.length);
+        setTimeout(showIt, 100);
+    }
+
+    runIt();
+
+    // showIt()
+
+
 
 })
-
-
