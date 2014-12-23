c425d22628806dffead0e3d3c49b21e44fec2926
jsAaron <401545553@qq.com>
2014-12-23 10:40:52 +0800

    样式快捷接口

---
+++ jsonp.php
@@ -0,0 +1,5 @@
+<?php
+    header('Content-Type:text/json;charset=utf-8');
+ //	echo trim( $_GET['callback']).'('. json_encode(array('status'=>1,'info'=>'OK')) .')';
+ 	echo json_encode(array('status'=>1,'info'=>'OK'));  
+?>
