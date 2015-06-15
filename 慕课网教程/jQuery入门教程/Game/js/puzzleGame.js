938437fb237b818409e4d2a621aaee7305f56eaa
jsAaron <401545553@qq.com>
2015-06-15 17:08:04 +0800

    puzzleGame

---
+++ 慕课网教程/jQuery入门教程/Game/js/puzzleGame.js
@@ -0,0 +1,28 @@
+
+/**
+ * 拼图游戏构造器
+ * @return {[type]} [description]
+ */
+function puzzleGame(contentArea, config) {
+
+	console.log(this)
+
+}
+
+
+
+
+
+puzzleGame.prototype = {
+
+	//开始游戏
+	startGame:function(){
+
+	},
+
+	//设置游戏的困难度
+	setDifficulty:function(){
+
+	}
+
+}
