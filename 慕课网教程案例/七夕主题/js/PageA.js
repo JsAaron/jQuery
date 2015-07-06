fa4686167aac9325ebfa0df634ad15d4aed58c85
jsAaron <401545553@qq.com>
2015-07-06 21:20:32 +0800

    精灵动画

--- 慕课网教程案例/七夕主题/js/PageA.js
+++ 慕课网教程案例/七夕主题/js/PageA.js
@@ -5,11 +5,26 @@
  */
 var PageA = function() {
 
-  
+
+    //鸟动画
+    $('#leftBird')
+        .sprite({
+            fps          : 9,
+            no_of_frames : 3
+        })
+        .spRandom({
+            top    : 50,
+            bottom : 200,
+            left   : 300,
+            right  : 320
+        })
+        .isDraggable()
+        .activeOnClick()
+        .active();
 
 
-	//执行云动画
-	CloudEffect()
+    //执行云动画
+    CloudEffect()
 
     //执行水波浪动画
     new WaveEffect("qx_water_1_1", -10, -1, 9, 1.5, 1.5)
