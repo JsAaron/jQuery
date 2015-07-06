6e1d527c236801fbae4a92cda45eaf419b6abac2
jsAaron <401545553@qq.com>
2015-07-06 16:57:04 +0800

    分离文件

---
+++ 慕课网教程案例/七夕主题/js/PageA.js
@@ -0,0 +1,29 @@
+
+/**
+ * 第一个页面
+ * @param  {[type]} argument [description]
+ * @return {[type]}          [description]
+ */
+var PageA = function() {
+
+
+
+
+	
+
+
+
+	//构建水波浪图
+	new WaveEffect("qx_water_1_1", -10, -1, 9, 1.5, 1.5)
+	new WaveEffect("qx_water_1_2", 20, -50, 7, 1.5, 1.2)
+	new WaveEffect("qx_water_1_3", 50, 7, 8, 0.8, 1.5)
+
+
+
+
+	return {
+		run: function() {
+
+		}
+	}
+}
--- 慕课网教程案例/七夕主题/js/Qixi.js
+++ 慕课网教程案例/七夕主题/js/Qixi.js
@@ -13,17 +13,14 @@ var Qixi = {
     init: function() {
 
         var container = document.getElementById('swipe');
-        var element = container.children[0];
-        var slides = element.children;
+        var element   = container.children[0];
+        var slides    = element.children;
 
         //页面滑动对象
         var swipe = Swipe(container);
 
-        new WaveEffect("qx_water_1_1", -10, -1, 9, 1.5, 1.5)
-        new WaveEffect("qx_water_1_2", 20, -50, 7, 1.5, 1.2)
-        new WaveEffect("qx_water_1_3", 50,7,8,0.8,1.5) 
-
-
+        //构建第一个页面
+        PageA()
 
         swipe.initComplete = function() {
             page1.start() //开始执行
