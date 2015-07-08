ff681f8273fed8e9e2a561150644b6ccb1e5201f
jsAaron <401545553@qq.com>
2015-07-08 10:05:27 +0800

    七夕主题

--- 慕课网教程案例/七夕主题/js/Qixi.js
+++ 慕课网教程案例/七夕主题/js/Qixi.js
@@ -6,97 +6,181 @@
  */
 var Qixi = {
 
-    //范围边界
-    range:{},
+    //实例对象
+    instance: {},
 
-    //尺寸
-    size:{},
+    //数据
+    data: {},
 
-    //计算出边界
-    //{0: 1, 1104: 1, 1105: 2, 2209: 2, 2210: 3, 3314: 3}
-    calculateRange:function(){
-        var i = Qixi.size.count;
-        while(i--){
-            Qixi.range[i * Qixi.size.width] = 'in:' + (i + 1)
-            Qixi.range[(i + 1) * Qixi.size.width - 1] = 'out:' + ( i + 1)
-        }
-        console.log(Qixi.range)
-    },
-
+    //初始化
     init: function() {
         var container = $("#content");
 
         //页面滑动对象
         var swipe = Swipe(container);
+        
+        //第一页页码
+        Qixi.data.width = container.width()
 
-        /////////
-        //执行云动画 //
-        //////////
-        var cloud = CloudEffect();
-        cloud.run()
-
-        //////////
-        // 小孩走路 //
-        //////////
-        var $girl = $("#girl");
-
-        //增加一个css3的效果动作变化
-        $girl.attr("class", "class charector-wrap slow")
-
-        //开始走路
-        var distance = container.width()
-        var startPox = distance / 2 - $girl.width() / 2
-
-        Qixi.size = {
-            width      : distance,
-            height     : container.height(),
-            startPox   : startPox,
-            count      : 3,
-            totalWidth : distance * 3
-        }
-        Qixi.calculateRange();
-
-        //用transition运行走动
-        $girl.transition({ 
-            left: startPox / 3 //第一次之运行1.5/3的距离
-        }, 7000, 'linear', function() {
-            //页面开始滚动
-            swipe.scrollTo(distance * 2, 30000)
-            //用transition继续运动
-            $girl.transition({
-                left: startPox
-            }, 30000, 'linear', function() {
-                console.log('人物到达中间')
-            });
-        });
+        //构建第一个场景
+        var qixiA = Qixi.createPage(1, {
+            container : container,
+            swipe     : swipe
+        })
 
 
         //监听动画变化
         swipe.watch('move', function(distance) {
-            // Qixi.disposeAction(distance)
+            Qixi.disposeAction(distance, qixiA.offset().left)
         })
 
         //监听动画完成
         swipe.watch('complete', function() {
-           
+
         })
     },
 
     //获取当前的页面
-    getCurrentPage:function(){
-
+    getCurrentPage: function(pageLeft, girlLeft) {
+        //实际移动的距离
+        var movePos = Math.abs(pageLeft) + girlLeft;
+        var pageIndex = Math.floor(movePos / Qixi.data.width)
+        return ++pageIndex //从1开始索引
     },
 
     //处理当前动作
-    disposeAction:function(distance){
+    disposeAction: function(pageLeft, girlLeft) {
+        //当前页码
+        var pageIndex = Qixi.getCurrentPage.apply(null, arguments);
+        //去重
+        if (pageIndex === Qixi.data.pageIndex) {
+            return;
+        }
+        //停止上一页动作
+        Qixi.stopAction(Qixi.data.pageIndex)
+            //创建新的页面
+        Qixi.createPage(pageIndex)
+    },
+
+    //停止动作
+    stopAction: function(pageIndex) {
+        Qixi.findInstance(pageIndex).stop();
+    },
+
+    //开始新的动作
+    createPage: function(pageIndex, options) {
+        var qixi;
+        //构建新的页面动作
+        function update(classNane) {
+            qixi = eval(classNane)(options)
+            Qixi.instance[pageIndex] = qixi;
+            qixi.run();
+        }
+        switch (pageIndex) {
+            case 1:
+                update('QixiA')
+                break
+            case 2:
+                update('QixiB')
+                break
+            case 3:
+                update('QixiC')
+                break
+        }
+        //更新页面
+        Qixi.data.pageIndex = pageIndex;
+        return qixi;
+    },
+
+    findInstance: function(pageIndex) {
+        return Qixi.instance[pageIndex]
+    }
+}
 
 
+/**
+ * 七夕场景一
+ * @type {[type]}
+ */
 
-        // console.log(distance,Qixi.range)
-
+var QixiA = function(options) {
+    var container = options.container;
+    var swipe = options.swipe;
+    //云动画 
+    var cloud = CloudEffect();
+    //走路对象
+    var $girl = $("#girl");
+    //开始走路
+    var width = container.width()
+    //目标中间位置
+    var middlePos = width / 2 - $girl.width() / 2;
+    //走路
+    function toWalk() {
+        //增加一个css3的效果动作变化
+        $girl.attr("class", "class charector-wrap slow")
+            //用transition运行走动
+        $girl.transition({
+            left: middlePos / 3 //第一次之运行1/3的距离
+        }, 700, 'linear', function() {
+            //页面开始滚动
+            swipe.scrollTo(width * 2, 5000)
+                //用transition继续运动
+            $girl.transition({
+                left: middlePos
+            }, 5000, 'linear', function() {
+                console.log('人物到达中间')
+            });
+        });
     }
 
+    return {
+        run: function() {
+            cloud.run()
+            toWalk();
+        },
+        stop: function() {
+
+        },
+        offset: function() {
+            return $girl.offset()
+        }
+    }
 }
+
+
+/**
+ * 七夕场景二
+ * @type {[type]}
+ */
+var QixiB = function() {
+
+    return {
+        run: function() {
+            alert('b')
+        },
+        stop: function() {
+
+        }
+    }
+}
+
+/**
+ * 七夕场景三
+ * @type {[type]}
+ */
+var QixiC = function() {
+
+    return {
+        run: function() {
+            //执行运动化
+            cloud.run()
+        },
+        stop: function() {
+
+        }
+    }
+}
+
 
 
 $(function() {
