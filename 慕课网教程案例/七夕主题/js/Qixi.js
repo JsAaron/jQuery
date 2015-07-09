7991a57913f9a13f738bf012f33ca89a0107210b
jsAaron <401545553@qq.com>
2015-07-09 16:30:09 +0800

    处理场景2

--- 慕课网教程案例/七夕主题/js/Qixi.js
+++ 慕课网教程案例/七夕主题/js/Qixi.js
@@ -27,12 +27,12 @@ var Qixi = {
 
         //走路
         var walk = QixiWalk(container)
-            walk.run(swipe);
+        walk.run(swipe);
 
         //构建第一个场景
         var qixiA = Qixi.createPage(Qixi.pageIndex, {
-            container : container,
-            swipe     : swipe
+            container: container,
+            swipe: swipe
         })
 
         //监听动画变化
@@ -64,7 +64,7 @@ var Qixi = {
         }
         //停止上一页动作
         Qixi.stopPage(Qixi.data.pageIndex)
-        //创建新的页面
+            //创建新的页面
         Qixi.createPage(pageIndex)
     },
 
@@ -104,31 +104,73 @@ var Qixi = {
     }
 }
 
+
+/**
+ * 商店
+ * @return {[type]} [description]
+ */
+var toShop = function() {
+
+    function doorAction(left, right, time, callback) {
+        var count = 2;
+        //等待开门完成
+        var complete = function() {
+            if (count == 1) {
+                callback();
+                return;
+            }
+            count--
+        }
+        $('.door-left').transition({
+            'left': '-50%'
+        }, time, complete)
+        $('.door-right').transition({
+            'left': '50%'
+        }, time, complete)
+    }
+
+    //开门
+    function openDoor(time, callback) {
+        doorAction('-50%', '50%', time, callback)
+    }
+
+    //关门
+    function shutDoor(time, callback) {
+        doorAction('0%', '0%', time, callback)
+    }
+
+    return {
+        //进去
+        in : function() {
+            //开门
+            openDoor(500,function(){
+                //开门成功
+                console.log(1)
+            });
+
+        },
+        //出来
+        out: function() {
+
+        }
+    }
+}
+
+
 /**
  * 走路动作
  */
 var QixiWalk = function(container) {
+
     //走路对象
     var $girl = $("#girl");
+
     //开始走路
     var width = container.width()
+
     //运动的位置
     var middlePos = width / 2 - $girl.width() / 2;
     var startPox = middlePos / 3 //第一次之运行1/3的距离
-
-    //得到鲜花
-    function flowers() {
-        //开始消失
-        $girl.transition({
-            opacity: 0
-        }, 2000)
-
-        setTimeout(function() {
-            $girl.transition({
-                opacity: 1
-            }, 2000)
-        }, 2000)
-    }
 
 
     //用transition做运动
@@ -136,7 +178,7 @@ var QixiWalk = function(container) {
         var dfd = $.Deferred();
         $girl.transition({
             left: pox
-        }, time, 'linear', function(){
+        }, time, 'linear', function() {
             dfd.resolve();
         });
         return dfd;
@@ -146,16 +188,29 @@ var QixiWalk = function(container) {
     function toWalk(swipe) {
         //增加一个css3的效果动作变化
         $girl.addClass('slowWalk')
-        //开始走路
-        var d1 = run(startPox, 1000);
-        //走路结束
+            //开始走路
+        var d1 = run(startPox, 100);
+        //第一段走路结束
         d1.done(function() {
             //开始滚动页面
-            swipe.scrollTo(width, 5000)
-            //继续走路
-            return run(middlePos, 5000)
-        }).done(function(){
-            console.log()
+            swipe.scrollTo(width, 500)
+                //继续走路
+            var d2 = run(middlePos, 500)
+                //第二段走路结束
+            d2.done(function() {
+
+                //停止走路
+                $girl.removeClass('slowWalk')
+                $girl.addClass('slowWalkFixed')
+
+                //去商店
+                var shop = toShop();
+                shop.in();
+
+
+
+
+            })
         })
     }
 
