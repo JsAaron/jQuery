ff0819355ba7b369102707258ecddb6e8f163c20
hy1404381881 <1404381881@qq.com>
2015-11-09 09:59:46 +0800

    11
    11

--- 测试案例/jsonp.php
+++
@@ -1,5 +0,0 @@
-<?php
-    header('Content-Type:text/json;charset=utf-8');
- //	echo trim( $_GET['callback']).'('. json_encode(array('status'=>1,'info'=>'OK')) .')';
- 	echo json_encode(array('status'=>1,'info'=>'OK'));  
-?>
